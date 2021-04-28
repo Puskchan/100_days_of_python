6ee337081e0a2dee4d094b8c59be2dcebb41b15a
Aditya Chandhoke <50488410+Puskchan@users.noreply.github.com>
2021-04-28 10:59:35 +0530

    Update main.py

--- 26(NATO_game)/main.py
+++ 26(NATO_game)/main.py
@@ -1,11 +1,18 @@
 import pandas
 
-usr_inp = [letter.upper() for letter in input("Enter a word: ")]
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 dict_data = {rows.letter: rows.code for (index, rows) in data.iterrows()}
 
-nato_words = [dict_data[i] for i in usr_inp if i in dict_data.keys()]
+def gen_pho():
+    usr_inp = input("Enter a word: ").upper()
+    try:
+        nato_words = [dict_data[i] for i in usr_inp]
+    except KeyError:
+        print("Please enter only alphabets!")
+        gen_pho()
+    else:
+        print(nato_words)
 
-print(nato_words)
+
+gen_pho()
